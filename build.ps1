b90192ed0f7c3d7eac1022f630855e5384f6ff85
bpedziwiatr <bpedziwiatr@sii.pl>
2022-01-28 10:39:01 +0100

    #31: update build script

--- build.ps1
+++ build.ps1
@@ -1,27 +1,28 @@
 function main() {
     $currentPath = Get-Location
-    rm .\publish
-    MSBuild DataverseGen.Cli/DataverseGen.Cli.csproj /t:Build /p:OutDir=$currentPath\publish /p:Configuration=Release /p:DebugType=None
+    Remove-Item .\publish
+    MSBuild DataverseGen.Cli/DataverseGen.Cli.csproj /t:Build /p:OutDir=$currentPath\publish\bin /p:Configuration=Release /p:DebugType=None
     
-    createTemplateDirectories
-    cd $currentPath
+    Set-Location $currentPath
     copyTemplates
+    MakeZip $currentPath
 }
 function copyTemplates() {
-    Write-Host $path
+    robocopy $currentPath"\DataverseGen.Core\Generators\Scriban\Templates\Multiple\Main\"  $currentPath"/publish/bin/Templates/ExampleTemplate/" *.sbncs /s
+    robocopy $currentPath"\DataverseGen.Core\Generators\Scriban\Templates\Multiple\TS-Main\"  $currentPath"/publish/bin/Templates/ExampleTSTemplate/" *.sbncs /s  
+}
+function getGitVersioion(){
+    $gitv = gitversion | ConvertFrom-JSON -ErrorAction stop
+    return $gitv
+}
+function MakeZip([string]$currentPath){
+    $gitv = getGitVersioion
+    $compress = @{
+        Path = $currentPath+"/publish/bin/*.*"
+        CompressionLevel = "Fastest"
+        DestinationPath = $currentPath+"/publish/DataverseGen-CLI_"+$gitv.NuGetVersionV2+".zip"
+      }
+      Compress-Archive @compress
+}
 
-    Copy-item * -destination c:\temp\targetdirectory -recurse`
-    robocopy $currentPath"\DataverseGen.Core\Generators\Scriban\Templates\Multiple\Main\"  $currentPath"/publish/Templates/ExampleTemplate/" *.sbncs /s
-    robocopy $currentPath"\DataverseGen.Core\Generators\Scriban\Templates\Multiple\TS-Main\"  $currentPath"/publish/Templates/ExampleTSTemplate/" *.sbncs /s
-    #Copy-item -Force -Recurse -Verbose $sourceDirectory -Destination $destinationDirectory
-    #     Copy-Item -Force  -Path $currentPath"\DataverseGen.Core\Generators\Scriban\Templates\Multiple\Main\"  -Recurse -Destination $currentPath"publish/Templates/ExampleTemplate/"
-    # Copy-Item  -Path $currentPath"\DataverseGen.Core\Generators\Scriban\Templates\Multiple\TS-Main\"  -Recurse -Destination $currentPath"publish/Templates/ExampleTemplateTS/"
-}
-function createTemplateDirectories() {
-    cd publish
-    md Templates
-    cd Templates
-    md ExampleTemplate
-    md ExampleTsTemplate
-}
 main
