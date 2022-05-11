cd72f49e5014420d54ee5d758dc4f0fdb962fe67
Bartosz Pedziwiatr-Ratajczak <bpedziwiatr@sii.pl>
2022-05-11 14:10:58 +0200

    ref: remove old t4template files feat: add build script

---
+++ build.ps1
@@ -0,0 +1,28 @@
+function main() {
+    $currentPath = Get-Location
+    Remove-Item .\publish -Recurse -ErrorAction Ignore    
+    MSBuild DataverseGen.Cli/DataverseGen.Cli.csproj /t:Build /p:OutDir=$currentPath\publish\bin /p:Configuration=Release /p:DebugType=None
+    
+    Set-Location $currentPath
+    copyTemplates
+    MakeZip $currentPath
+}
+function copyTemplates() {
+    robocopy $currentPath"\DataverseGen.Core\Generators\Scriban\Templates\Multiple\Main\"  $currentPath"/publish/bin/Templates/ExampleTemplate/" *.sbncs /s
+    robocopy $currentPath"\DataverseGen.Core\Generators\Scriban\Templates\Multiple\TS-Main\"  $currentPath"/publish/bin/Templates/ExampleTSTemplate/" *.sbncs /s  
+}
+function getGitVersioion(){
+    $gitv = dotnet-gitversion | ConvertFrom-JSON -ErrorAction stop
+    return $gitv
+}
+function MakeZip([string]$currentPath){
+    $gitv = getGitVersioion
+    $compress = @{
+        Path = $currentPath+"/publish/bin/**"
+        CompressionLevel = "Fastest"
+        DestinationPath = $currentPath+"/publish/DataverseGen-CLI_"+$gitv.NuGetVersionV2+".zip"
+      }
+      Compress-Archive @compress
+}
+
+main
