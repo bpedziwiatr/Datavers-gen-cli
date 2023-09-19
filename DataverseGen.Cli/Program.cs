caac37d27c84a827bbf96d1b9035e3e79e7f17de
Kamil Skoracki <C485@users.noreply.github.com>
2023-09-19 08:32:10 +0200

    feat(23): When ConnectionString in config is empty, prompt for Credential will be displayed and CredentialManager will be used for password/connectionstring storage

--- DataverseGen.Cli/Program.cs
+++ DataverseGen.Cli/Program.cs
@@ -1,20 +1,24 @@
 ﻿using System.Diagnostics;
+using CommunityToolkit.Diagnostics;
 using DataverseGen.Core.Config;
 using DataverseGen.Core.DataConverter;
 using DataverseGen.Core.Generators.Scriban;
 using DataverseGen.Core.Metadata;
+using Meziantou.Framework.Win32;
 using Newtonsoft.Json;
 using static DataverseGen.Core.ColorConsole;
+
 namespace DataverseGen.Cli;
+
 //*
 // NOTE!!
 // DO NOT USE 'Trim unused assemblies'! It'll break json deserialization!
 //*//
 internal static class Program
 {
-    private const string DataverseGenConfigJson = "dataversegen.config.json";
+	private const string DataverseGenConfigJson = "dataversegen.config.json";
 
-    private const string Title = @"
+	private const string Title = @"
     ____        __       _    __                       ______         
    / __ \____ _/ /_____ | |  / /__  _____________     / ____/__  ____ 
   / / / / __ `/ __/ __ `/ | / / _ \/ ___/ ___/ _ \   / / __/ _ \/ __ \
@@ -22,91 +26,130 @@ internal static class Program
 /_____/\__,_/\__/\__,_/ |___/\___/_/  /____/\___/   \____/\___/_/ /_/                                                                       
 ";
 
-    private static ConfigModel GetConfig()
-    {
-        if (!File.Exists(DataverseGenConfigJson))
-        {
-            throw new FileNotFoundException("Config not found in current directory",
-                DataverseGenConfigJson);
-        }
+	private static ConfigModel GetConfig()
+	{
+		if (!File.Exists(DataverseGenConfigJson))
+		{
+			throw new FileNotFoundException("Config not found in current directory",
+				DataverseGenConfigJson);
+		}
 
-        string json = File.ReadAllText(DataverseGenConfigJson);
-        return JsonConvert.DeserializeObject<ConfigModel>(json);
-    }
+		string json = File.ReadAllText(DataverseGenConfigJson);
+		ConfigModel? deserializedObject = JsonConvert.DeserializeObject<ConfigModel>(json);
+		Guard.IsNotNull(deserializedObject);
 
-    private static void Main(string[] args)
-    {
-        try
-        {
-            Console.SetWindowSize(140, 30);
-            WriteLine(Title);
-            WriteVersion();
+		if (deserializedObject.ConnectionString is not (null or ""))
+		{
+			return deserializedObject;
+		}
 
-            ConfigModel config = GetConfig();
+		string credentialKey = $"DataverseGen-{deserializedObject.Namespace}";
+		Credential? creds = CredentialManager.ReadCredential(credentialKey);
 
-            DataverseMetadataConverter dataConverter = new(
-                new DataverseConnector
-                (config.ConnectionString,
-                    config.EnableConnectionStringValidation),
-                config.ThrowOnEntityNotFound,
-                config.Entities
-            );
-            MappingEntity[] data = dataConverter.GetMappedEntities();
-            WriteLine(@"Finish Load data");
-            Context context = new()
-            {
-                Namespace = config.Namespace,
-                Entities = data
-            };
+		if (creds is null)
+		{
+			CredentialResult? newCreds = null;
 
-            WriteLine(@"Start generator");
-            switch (config.TemplateEngine.Name)
-            {
-                case "scriban":
-                    ScribanRun(config, context);
-                    break;
-                default:
-                    WriteLine(
-                        $@"Unsupported generator {config.TemplateEngine}, run scriban");
-                    ScribanRun(config, context);
-                    break;
-            }
-        }
-        catch (Exception e)
-        {
-            WriteError(e.ToString());
-            WriteError(@"#StackTrace:");
-            WriteError(e.StackTrace);
-            throw;
-        }
-        finally
-        {
-            WriteSuccess(@"Bye Bye, see you next time Press any Key to exit");
-            Console.ReadKey();
-        }
-    }
+			while (newCreds is null)
+			{
+				newCreds = CredentialManager.PromptForCredentials(captionText:"New connection string prompt",
+					messageText:$"Provide valid connection string for {credentialKey}",
+					userName:"DO NOT USE",
+					saveCredential:CredentialSaveOption.Hidden);
+			}
 
-    private static void WriteVersion()
-    {
-        ProcessModule? assembly = Process.GetCurrentProcess().MainModule;
+			CredentialManager.WriteCredential(credentialKey,
+				"UNUSED",
+				newCreds.Password,
+				CredentialPersistence.LocalMachine);
 
-        if (assembly is null)
-        {
-            WriteLine("Unable to get main module version.");
-            return;
-        }
-        FileVersionInfo assemblyFileVersion = assembly.FileVersionInfo;
-        WriteLine($"Runtime Version: {assemblyFileVersion.FileVersion}");
-    }
+			deserializedObject.ConnectionString = newCreds.Password;
 
-    private static void ScribanRun(ConfigModel config, Context context)
-    {
-        ScribanGenerator scribanGenerator = new(
-            config.TemplateName,
-            config.TemplateDirectoryName,
-            config.OutDirectory,
-            context,
-            config.TemplateEngine);
-        scribanGenerator.GenerateTemplate();
-    }
+			return deserializedObject;
+		}
+
+		deserializedObject.ConnectionString = creds.Password;
+
+		return deserializedObject;
+	}
+
+	private static void Main(string[] args)
+	{
+		try
+		{
+			Console.SetWindowSize(140, 30);
+			WriteLine(Title);
+			WriteVersion();
+
+			ConfigModel config = GetConfig();
+
+			DataverseMetadataConverter dataConverter = new(new DataverseConnector(config.ConnectionString,
+					config.EnableConnectionStringValidation),
+				config.ThrowOnEntityNotFound,
+				config.Entities);
+
+			MappingEntity[] data = dataConverter.GetMappedEntities();
+			WriteLine(@"Finish Load data");
+			Context context = new()
+			{
+				Namespace = config.Namespace,
+				Entities = data
+			};
+
+			WriteLine(@"Start generator");
+
+			switch (config.TemplateEngine.Name)
+			{
+				case "scriban":
+					ScribanRun(config, context);
+
+					break;
+
+				default:
+					WriteLine($@"Unsupported generator {config.TemplateEngine}, run scriban");
+					ScribanRun(config, context);
+
+					break;
+			}
+		}
+		catch (Exception e)
+		{
+			WriteError(e.ToString());
+			WriteError(@"#StackTrace:");
+			WriteError(e.StackTrace);
+
+			throw;
+		}
+		finally
+		{
+			WriteSuccess(@"Bye Bye, see you next time Press any Key to exit");
+			Console.ReadKey();
+		}
+	}
+
+	private static void WriteVersion()
+	{
+		ProcessModule? assembly = Process.GetCurrentProcess().MainModule;
+
+		if (assembly is null)
+		{
+			WriteLine("Unable to get main module version.");
+
+			return;
+		}
+
+		FileVersionInfo assemblyFileVersion = assembly.FileVersionInfo;
+		WriteLine($"Runtime Version: {assemblyFileVersion.FileVersion}");
+	}
+
+	private static void ScribanRun(ConfigModel config, Context context)
+	{
+		ScribanGenerator scribanGenerator = new(config.TemplateName,
+			config.TemplateDirectoryName,
+			config.OutDirectory,
+			context,
+			config.TemplateEngine);
+
+		scribanGenerator.GenerateTemplate();
+	}
 }
