91f92983c56bf73c0a6473170998d079d4566d06
Bartosz Pedziwiatr-Ratajczak <bpedziwiatr@sii.pl>
2023-10-26 09:03:45 +0200

    fix

--- Tests/DataverseGen.Core.Tests/ConnectionString/ClientSecretTests.cs
+++ Tests/DataverseGen.Core.Tests/ConnectionString/ClientSecretTests.cs
@@ -11,7 +11,7 @@ namespace DataverseGen.Core.Tests.ConnectionString
   url=https://contosotest.crm.dynamics.com;
   ClientId={AppId};
   ClientSecret={ClientSecret}")]
-        [DataRow("AuthType=ClientSecret;url=https://rwrgbsmdrsdev.crm4.dynamics.com/main.aspx?appid=bcedab73-941b-ec11-b6e6-000d3adbbffa;ClientId=27aa45a2-7b10-15f3-b123-53b53de124e1;ClientSecret=vz3246578*&*&!;")]
+        [DataRow("AuthType=ClientSecret;url=https://contosotest.crm4.dynamics.com/main.aspx?appid=cbeoar26-931b-ec11-b6e6-020d3adbbfda;ClientId=6702e612-b6a9-4eed-bfab-967d5565df69;ClientSecret=vz3246578*&*&!;")]
 
         public void ClientSecret_ConnectionString_Success(string connectionString)
         {
